2b40837df3ecccbddcbd8d49df6f4adf5cc00fe5
freedom <1563607296@qq.com>
2017-04-09 23:46:18 +0800

    完成轮次榜的ui

--- src/components/news/News.vue
+++ src/components/news/News.vue
@@ -4,23 +4,67 @@
         <div class="roundList">
             <div class="list-head"></div>
             <div class="list-body">
-                <div class="list-item">
-                    <span class="title">第1轮</span>
+                <div class="list-item" v-for="(index, item) in roundList">
+                    <span class="title">第{{index + 1}}轮</span>
                     <div class="content">
-                        <div>
-                            <img class="avator" src="../../assets/images/list/yy.png" onerror="this.src='../../assets/images/list/yy.png'" />
+                        <div class="champion">
+                            <img class="avator" src="//s1.yy.com/guild/header/10001.jpg" onerror="this.src='//s1.yy.com/guild/header/10001.jpg'" />
                             <i class="duanwei">暂无段位</i>
                         </div>
+                        <table>
+                            <tr>
+                                <td>
+                                    <p><span class="nick">虚位以待虚位以待虚位以待虚位以待虚位以待</span><i class="name">头条</i></p>
+                                    <span class="value">闪耀值：<em>798</em></span>
+                                    <i class="stars"></i>
+                                </td>
+                                <td>
+                                    <i class="people"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p><span class="nick">虚位以待虚位以待虚位以待虚位以待虚位以待</span><i class="name">头条</i></p>
+                                    <span class="value">闪耀值：<em>798</em></span>
+                                    <i class="stars"></i>
+                                </td>
+                                <td>
+                                    <i class="people"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p><span class="nick">虚位以待虚位以待虚位以待虚位以待虚位以待</span><i class="name">头条</i></p>
+                                    <span class="value">闪耀值：<em>798</em></span>
+                                    <i class="stars"></i>
+                                </td>
+                                <td>
+                                    <i class="people"></i>
+                                </td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td>
+                                    <img class="avator" src="//s1.yy.com/guild/header/10001.jpg" onerror="this.src='//s1.yy.com/guild/header/10001.jpg'" />
+                                </td>
+                                <td>
+                                    <span class="nickname">虚位以待</span>
+                                    <span class="value">影响力：<em>798</em></span>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- <div class="yellowBlank"></div> -->
         <!-- 我的头条 -->
-
+        <!-- <div class="yellowBlank"></div> -->
         <!-- 头条擂主 -->
-
+        <!-- <div class="yellowBlank"></div> -->
         <!-- 头条段位榜 -->
-
+        <!-- <div class="yellowBlank"></div> -->
         <!-- Y阅八卦 -->
 	</div>
 </template>
@@ -29,7 +73,11 @@
 import Vue from 'vue'; 
 export default {
     data:function(){
-        return {}
+        let roundList = [];
+        for(let i = 0; i < 4; i++){
+            roundList[i] = {};
+        }
+        return {roundList: roundList}
      },
      ready:function(){
         let $newsBtn = $('.newsBtn');
@@ -40,5 +88,5 @@ export default {
 </script>
 <style scoped lang="scss">
 @import '../../assets/sass/base/_mixin.scss';
-@import '../../assets/sass/rule.scss';
+@import '../../assets/sass/news.scss';
 </style>
